e2476c0f1f55fbfcfa28c83970fe041ec0355958
momoyi <moyi.abioye@gmail.com>
2024-10-17 16:50:59 +0100

    Update archive.js

--- archive.js
+++ archive.js
@@ -311,4 +311,4 @@ function initPage() {
   document.addEventListener("resize", calculateIntroHeight());
 }
 
-document.addEventListener("DOMContentLoaded", initPage());
+window.addEventListener("runsWhenReady", initPage());
